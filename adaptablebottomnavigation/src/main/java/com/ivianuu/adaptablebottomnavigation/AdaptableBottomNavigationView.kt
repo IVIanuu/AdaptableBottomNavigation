ae842acf906ad28b171010a2fdb8e3e7b870350a
IVIanuu <IVIanuu@gmail.com>
2018-02-17 13:27:49 +0100

    Removed logging

--- adaptablebottomnavigation/src/main/java/com/ivianuu/adaptablebottomnavigation/AdaptableBottomNavigationView.kt
+++ adaptablebottomnavigation/src/main/java/com/ivianuu/adaptablebottomnavigation/AdaptableBottomNavigationView.kt
@@ -22,12 +22,11 @@ import android.os.Parcelable
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.view.ViewPager
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MenuItem
 import kotlinx.android.parcel.Parcelize
 
 fun Any.d(message: () -> String) {
-    Log.d(this::class.java.simpleName, message())
+//    Log.d(this::class.java.simpleName, message())
 }
 
 /**
